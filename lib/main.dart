f213580d0f4c6e121f96dcc6f19d19bb689234cb
Emdad Hossain <emdadhossain25@users.noreply.github.com>
2026-01-06 17:51:58 +0600

    removed commendts using ctrl+R

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false, // show debug checked mode banner flag
+      debugShowCheckedModeBanner: false,
       title: 'Ticket App',
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       home: const MyHomePage(title: 'Ticket App'),
